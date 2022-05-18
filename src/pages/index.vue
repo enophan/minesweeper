c20db537d119a9565c94ae90dcb06b091ef0b166
enophan <zhaooooooyifan@qq.com>
2022-05-18 21:32:19 +0800

    chore: update

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,5 +3,13 @@
 </script>
 
 <template>
-  <div />
+  <div>
+    Minesweeper
+
+    <div v-for="y in 10" :key="y">
+      <button v-for="x in 10" :key="x" w-10 h-10 border>
+        {{ y * 10 + x }}
+      </button>
+    </div>
+  </div>
 </template>
