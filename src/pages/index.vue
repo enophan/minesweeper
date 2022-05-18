bd55ac6b32761a16dd4e5fdd7c4c41322af57d33
enophan <zhaooooooyifan@qq.com>
2022-05-18 21:39:56 +0800

    chore: update

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,9 +6,19 @@
   <div>
     Minesweeper
 
-    <div v-for="y in 10" :key="y">
-      <button v-for="x in 10" :key="x" w-10 h-10 border>
-        {{ y * 10 + x }}
+    <div
+      v-for="y in 10"
+      :key="y"
+    >
+      <button
+        v-for="x in 10"
+        :key="x"
+        w-10
+        h-10
+        hover:bg-gray
+        border
+      >
+        {{ (y - 1) * 10 + x }}
       </button>
     </div>
   </div>
